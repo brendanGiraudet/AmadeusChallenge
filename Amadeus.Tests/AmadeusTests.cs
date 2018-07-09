9cc07265ba6966eb17b41199201e41997e08404f
nicolas stophe <nicolas.stophe@live.com>
2018-07-09 22:56:22 +0200

    Premier test fonctionnel

--- Amadeus.Tests/AmadeusTests.cs
+++ Amadeus.Tests/AmadeusTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Amadeus;
+using System;
+using System.Collections.Generic;
 
 namespace Amadeus.Tests
 {
@@ -13,12 +15,50 @@ namespace Amadeus.Tests
             game = new Game();
             game.PlanetCount = 34;
             game.EdgeCount = 59;
+            game.Edges = new List<Edge>();
+            game.Planets = new List<Planet>();
         }
 
         [TestMethod]
         public void TestMethod1()
         {
+            Init("Test1.txt");
+
             Assert.IsTrue(true);
+        }
+
+        private void Init(string fileName)
+        {
+            int counter = 0;  
+            string line;
+            string[] inputs;  
+            System.IO.StreamReader file =   
+                new System.IO.StreamReader(@"..\..\..\..\Amadeus.Tests\" + fileName);  
+            while((line = file.ReadLine()) != null)  
+            {
+                if(counter == 0)
+                {
+                    inputs = line.Split(' ');
+                    game.PlanetCount = int.Parse(inputs[0]);
+                    game.EdgeCount = int.Parse(inputs[1]);
+                }  else if (counter < game.EdgeCount + 1) {
+                    inputs = line.Split(' ');
+                    game.Edges.Add( new Edge { PlanetA = int.Parse(inputs[0]),
+                        PlanetB = int.Parse(inputs[1])});
+                } else {
+                    inputs = line.Split(' ');
+                    game.Planets.Add( new Planet {
+                        MyUnits = int.Parse(inputs[0]),
+                        MyTolerance = int.Parse(inputs[1]),
+                        OtherUnits = int.Parse(inputs[2]),
+                        OtherTolerance = int.Parse(inputs[3]),
+                        CanAssign = int.Parse(inputs[4])
+                    });
+                }
+                counter++;
+            }  
+
+            file.Close();  
         }
     }
 }
